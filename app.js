16d5116bf69a026c37b069ff6dea39845d9900b1
Ezego1 <ezegodwin24@yahoo.com>
2020-07-26 02:33:43 +0100

    Add modal popup funtionality

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    const baseUrl = 'https://swapi.dev/api/people/'
+document.addEventListener('DOMContentLoaded', async() => {
+    try {
+        const baseUrl = 'https://swapi.dev/api/people/'
     
-    let display = document.querySelector('.gallery')
+        let card = document.querySelector('.gallery')
+        
+        const response = await fetch(baseUrl)
     
-    fetch(baseUrl)
-    .then((response) => {
         if (!response.ok) {
             throw Error(response.statusText)
         }
-        response.json()
-        .then((data) => {
-            console.log(data)
-            let actors = data.results
-            actors.map(actor => {
-                const htmlElement = `        
-                <div class="span-1">
-                <img src="./resources/actors/actor1.jpg" alt="actor picture">
-                <p>${actor.name}</p>
-                </div>  
-            `
-            display.innerHTML += htmlElement
-            });
+        
+        const data = await response.json()
+        let {results} = data
+        console.log(data)
+        results.map((actor, index) => {
+            const htmlElement = `        
+            <a href="#modal">
+            <div class="span-1">
+                <img src="./resources/actors/actor1.jpg" alt="">
+                <p id='${index}'>${actor.name}</p>
+            </div> 
+            </a>`
+
+            card.innerHTML += htmlElement
+        });
+    
+    } catch (error) {
+        console.log(error)
+    }
+
+    class User {
+        constructor(name, gender, age, height) {
+            this.name = name
+            this.gender = gender
+            this.age = age 
+            this.height = height
+        }
+
+        getDetails() {
+
+        }
+    }
+
+    document.querySelectorAll('.span-1').forEach((el, index) => {
+        el.addEventListener('click', () => {
+            name = document.getElementById(index).innerHTML
+            console.log(name)
         })
     })
-    .catch((e) => console.log(e))
-    
-    console.log(display)
+
 })
